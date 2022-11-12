7fab9f239a00745a53369b84b090db5144aee16d
k1nase <117951265+k1nase@users.noreply.github.com>
2022-11-12 19:09:25 +0100

    Update polybar_conf.sh

--- polybar_conf.sh
+++ polybar_conf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+B=$(tput bold)
+N=$(tput sgr0)
 
 echo "${B}###Configuration polybar###${N}"
-git clone https://github.com/k1nase/polybar
 
 cp ~/polybar/launch.sh ~/.config/polybar
-chmod +x ~/.config/polybar/lauch.sh
+chmod +x ~/.config/polybar/launch.sh
 
 cp ~/polybar/config ~/.config/polybar
