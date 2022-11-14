276d1e8481939834a125085d06e93b693a318478
k1nase <117951265+k1nase@users.noreply.github.com>
2022-11-15 00:14:04 +0100

    Update polybar_icons.sh

--- polybar_icons.sh
+++ polybar_icons.sh
@@ -1,6 +1,8 @@
 #!bin/bash
 
-
+#Icon + themes
 #Icons : https://arcolinux.com/where-do-i-find-icons-for-polybar-how-do-i-change-them/
 
 sudo add-apt-repository ppa:font-manager/staging && sudo apt-get update && sudo apt-get install font-manager && sudo apt-get install fonts-font-awesome 
+
+sudo apt install sddm-theme-breeze
