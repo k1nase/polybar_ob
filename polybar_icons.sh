9a0d8a7211654caeb6fbb2ad85bab0597faa30c8
k1nase <117951265+k1nase@users.noreply.github.com>
2022-11-14 23:28:14 +0100

    Update polybar_icons.sh

--- polybar_icons.sh
+++ polybar_icons.sh
@@ -3,4 +3,4 @@
 
 #Icons : https://arcolinux.com/where-do-i-find-icons-for-polybar-how-do-i-change-them/
 
-sudo add-apt-repository ppa:font-manager/staging && sudo apt-get update && sudo apt-get install font-manager
+sudo add-apt-repository ppa:font-manager/staging && sudo apt-get update && sudo apt-get install font-manager && sudo apt-get install fonts-font-awesome 
